b176164d9d55d9cb9b23dd29d9f84cb9ea40533e
raphsilva <raphaelsilva2500@gmail.com>
2019-09-11 09:32:00 -0300

    Removed commented out code from directory Kim

--- Kim/setup.py
+++ Kim/setup.py
@@ -1,12 +1,8 @@
-# <------------------------>
-# <-----> PAR <----->
-# <------------------------>
 
 SUMLEN = None # Summary length (number of sentences on each side). Set 'None' for automatic.
 
 MAX_WORDS = 2*100
 
-#PAR = dict()
 LANGUAGE = 'portuguese'
 LAMBDA = '0.5'
 METHOD = 'CF' 
@@ -19,12 +15,6 @@ DATASET_ID = 8
 
 
 ASPECTS_TAGS = ['none', 'join', 'only'][0]
-
-
-
-# Products IDs 
-#SOURCE1 = '10'    
-#SOURCE2 = '11'   
 
 SOURCE1, SOURCE2= [ (), ('D1a','D1b'), ('D2a','D2b'), ('D3a','D3b'), ('D4a','D4b'), ('D5a','D5b'), ('D6a','D6b'), ('D7a','D7b'), ('D8a','D8b')][DATASET_ID]
 
@@ -41,20 +31,7 @@ SIZE_FAC['only']['CF'] = [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7]
 SIZE_FAC['only']['RF'] = [0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7]
 
 
-#PAR['ASPECT'] = 'short'
-#ASPECT = PAR['ASPECT']
-
-#if ASPECT != '':
-    #FILENAME1 = SOURCE1+'-'+ASPECT
-    #FILENAME2 = SOURCE2+'-'+ASPECT
-#else:
-    #FILENAME1 = SOURCE1
-    #FILENAME2 = SOURCE2
-
 DATAPATH = '../input'
-
-#PAR[SOURCE1] = PAR['DATAPATH'] + '/' + ASPECT + '_' + PAR['ID1'] + '.txt'
-#PAR[SOURCE2] = PAR['DATAPATH'] + '/' + ASPECT + '_' + PAR['ID2'] + '.txt'
 
 
 SHOW_SUMMARY = False
@@ -63,17 +40,5 @@ SHOW_INDEXES = False
 
 
 
-#DATA_DIR = PAR['DATAPATH']
-# Get full path for the file with data of target 
 def filepath(target):
-    #if ASPECT == '':
-        #return DATA_DIR+'/'+target+'.json'  
-    #else:
-        #return DATA_DIR+'/'+target+'-'+ASPECT+'.json'
     return DATAPATH +'/'+target+'.json'
-
-
-#PAR = dict()
-#PAR['LANGUAGE'] = LANGUAGE
-
-#PAR['ALLOW_REPETITION'] = ALLOW_REPETITION
