2e5afdf2f58aa7adf5b69d747deab49aafef5522
raphsilva <raphaelsilva2500@gmail.com>
2019-09-11 09:41:49 -0300

    Reformated files

--- Raphael/writefiles.py
+++ Raphael/writefiles.py
@@ -2,8 +2,7 @@
 
 
 import os.path
-import json     
-
+import json
 
 TRASH_DIR = 'trash'
 
@@ -11,35 +10,38 @@ TRASH_DIR = 'trash'
 def mk_dir(path):
     if not os.path.exists(path):
         os.makedirs(path)
-        
+
+
 def sep_dir_file(filepath):
     s = filepath.split('/')
     filename = ''.join(s[-1])
-    dirname = '/'.join(s[:-1])+'/'
+    dirname = '/'.join(s[:-1]) + '/'
     if dirname == '/':
         dirname = './'
     return dirname, filename
 
+
 def remove_dir(dirname):
     if os.path.isdir(dirname) == False:
         return
-    new_name = TRASH_DIR+'/'+dirname
+    new_name = TRASH_DIR + '/' + dirname
     while os.path.isdir(new_name) == True:
         new_name += '-'
     os.rename(dirname, new_name)
-    
+
 
 def remove_old_file(path, filename):
-    if os.path.isfile(path+'/'+filename) == False:
-        return        
-    new_path = TRASH_DIR+'/' + path
+    if os.path.isfile(path + '/' + filename) == False:
+        return
+    new_path = TRASH_DIR + '/' + path
     mk_dir(new_path)
     new_name = filename
     while os.path.isfile(new_path + '/' + new_name) == True:
         new_name += '-'
-    #print('Will rename %s to %s' % (path+'/'+filename, new_path+'/'+new_name))
-    #input()
-    os.rename(path+'/'+filename, new_path+'/'+new_name) 
+    # print('Will rename %s to %s' % (path+'/'+filename, new_path+'/'+new_name))
+    # input()
+    os.rename(path + '/' + filename, new_path + '/' + new_name)
+
 
 def load_file(path):
     if os.path.isfile(path) == False:
@@ -47,23 +49,25 @@ def load_file(path):
     r = open(path, 'r')
     return r
 
+
 def save_to_file(filepath, data, mode='pretty'):
     dirname, filename = sep_dir_file(filepath)
     mk_dir(dirname)
-    remove_old_file(dirname,filename)
+    remove_old_file(dirname, filename)
     underwrite_file(filepath, data, mode)
-    
-    
+
+
 def underwrite_file(filepath, data, mode='pretty'):
     dirname, filename = sep_dir_file(filepath)
     mk_dir(dirname)
-    f = open(dirname+filename, 'w')
-    if mode == 'pretty':         
-        f.write(json.dumps(data,sort_keys=True,indent=4, separators=(',', ': '), ensure_ascii=False))
+    f = open(dirname + filename, 'w')
+    if mode == 'pretty':
+        f.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False))
     else:
-        f.write(str(data).replace('\'','\"'))
+        f.write(str(data).replace('\'', '\"'))
     f.close()
-    
+
+
 def get_variable_from_file(filepath):
     g = load_file(filepath)
     if g == False:
@@ -72,8 +76,6 @@ def get_variable_from_file(filepath):
         return json.load(g)
     except:
         return False
-    
-    
-    
-    
-mk_dir(TRASH_DIR)    
+
+
+mk_dir(TRASH_DIR)
