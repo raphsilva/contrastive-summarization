9b4dd3e8abe7284a339398db3cfbfa5e3604066e
raphsilva <you@example.com>
2019-09-11 09:32:00 -0300

    Removed commented out code from directory Kim

--- Kim/similarity.py
+++ Kim/similarity.py
@@ -1,6 +1,5 @@
 from language import removeNegsAndAdjs
 
-
 # in order to gain efficiency timewise, if a ϕ or ψ is calculated, it's stored
 # so it wouldn't be necessary to calculate it again
 cache_phi = {}
@@ -14,7 +13,7 @@ def omega(u, v):
     :param v: [str] word to be compared.
     :return: [float] value in range [0, 1].
     """
-    
+
     # using Word Overlap
     if u == v:
         return 1
@@ -22,22 +21,16 @@ def omega(u, v):
         return 0
 
 
-
-
-
-
-
 def phi(text_info_1, text_info_2):
     global cache_phi
     """
     Given two opinionated sentences with the same polarity, the content similarity function φ(s1,s2) ∈ [0,1] measures the overall content similarity of s1 and s2.
     """
-    
+
     if len(text_info_1) * len(text_info_2) == 0:
         return 0
-    
-    idx = str(text_info_1)+'::'+str(text_info_2)
-    
+
+    idx = str(text_info_1) + '::' + str(text_info_2)
 
     # if ϕ(sentence1, sentence2) isn't calculated yet
     if idx not in cache_phi.keys():
@@ -59,15 +52,7 @@ def phi(text_info_1, text_info_2):
 
         cache_phi[idx] = float(sum_sentence1 + sum_sentence2) / (len(text_info_1) + len(text_info_2))
 
-    #print(text_info_1)
-    #print(text_info_2)
-    #print(cache_phi[idx])
-    #print()
-    #input()
-
     return cache_phi[idx]
-
-
 
 
 def psi(text_info_1, text_info_2):
@@ -75,19 +60,16 @@ def psi(text_info_1, text_info_2):
     Given two opinionated sentences s1 and s2 with opposite polarities, the contrastive similarity function ψ(s1,s2) ∈ [0,1] measures the similarity of s1 and s2 excluding their difference in sentiment.
     """
 
-
     if len(text_info_1) * len(text_info_2) == 0:
         return 0
-    
+
     text_info_1 = removeNegsAndAdjs(text_info_1)
     text_info_2 = removeNegsAndAdjs(text_info_2)
-    
-    idx = str(text_info_1)+'::'+str(text_info_2)
-    
+
+    idx = str(text_info_1) + '::' + str(text_info_2)
+
     r = phi(text_info_1, text_info_2)
 
     cache_psi[idx] = r
 
     return r
-
-
