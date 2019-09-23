d91237214ba1f52b2849835c97b4256cfbf7a18d
raphsilva <raphaelsilva2500@gmail.com>
2019-09-22 22:05:23 -0300

    Moved header to readme.md

--- Kim/main.py
+++ Kim/main.py
@@ -1,13 +1,3 @@
-"""
-    Developed by: Otávio Augusto Ferreira Sousa (June 2018)    
-    Adapted by: Raphael Rocha da Silva (September 2018) 
-    Last modified: October, 2018
-    
-    This file contains the implementation of a Contrastive Opinion Summarizer based on the work of Kim & Zhai(2009).
-    Hyun Duk Kim and ChengXiang Zhai, Generating Comparative Summaries of Contradictory Opinions in Text, CIKM 2009
-
-"""
-
 import evaluation as evalu
 import output_format as out
 from interface import *
