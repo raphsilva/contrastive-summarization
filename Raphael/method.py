d7c571885027470ce2e23c5b04dd47dd4165d247
raphsilva <raphaelsilva2500@gmail.com>
2019-10-05 14:17:58 -0300

    Removed unused language methods.

--- Raphael/method.py
+++ Raphael/method.py
@@ -6,8 +6,6 @@ from setup import KEY_MEASURE
 
 import re, string
 
-from language import getSentimentLexicon
-from language import negation_words
 
 from writefiles import underwrite_file
 from writefiles import get_variable_from_file
@@ -22,60 +20,6 @@ def set_ALPHA(value):
 def divergence_measure(d1, d2):
     return KLdivergence(d1, d2)
 
-
-possible_aspects = []
-aspects_list = get_variable_from_file('language/portuguese/aspects.json')
-for typ in aspects_list:
-    for k in aspects_list[typ]:
-        possible_aspects.append(k)
-
-sentiment_lexicon = getSentimentLexicon()
-
-
-def lex_sent(term):
-    term = term.lower()
-    term = re.sub('[' + string.punctuation + ']', '', term)  # Remove punctuation
-    if POLARITY_ATTRIBUTION == 'complex':
-        if term in possible_aspects:
-            return 0
-    r = 0
-    if term in sentiment_lexicon:
-        r = sentiment_lexicon[term]
-
-    # print('  ', term, r)
-    return MAXPOLARITY * r
-
-
-def find_polarities(words):
-    if POLARITY_ATTRIBUTION == 'pure':
-
-        return [(i, lex_sent(i)) for i in words]
-
-    elif POLARITY_ATTRIBUTION == 'complex':
-
-        if any(i in negation_words for i in words):
-
-            l = []
-
-            neg_pos = []
-
-            for i in range(len(words)):
-                if words[i] in negation_words:
-                    neg_pos.append(i)
-
-            for i in range(len(words)):
-                if i in neg_pos:
-                    continue  # won't keep negation words
-                if any(i - j <= 3 and i - j > 0 for j in neg_pos):
-                    l.append((words[i], -lex_sent(words[i])))
-
-                else:
-                    l.append((words[i], lex_sent(words[i])))
-
-        else:
-            l = [(i, lex_sent(i)) for i in words]
-
-        return l
 
 
 def sent(words_polarities):
@@ -98,15 +42,6 @@ def OLD_mean_summ_sent(S, info):
         r += sent(info[i]['sentence'])
     r = r / len(S)
     return int(r)
-
-
-def mismatch(S, S_rating, info):
-    a = S_rating - mean_summ_sent(S, info)
-    return a * a / MAXPOLARITY
-
-
-def SM(source, candidate):
-    return mismatch(source, candidate)
 
 
 cache_distance = get_variable_from_file('cache/distance.cache')
