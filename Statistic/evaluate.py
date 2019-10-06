bf6ed9f826d91e53bf05be1d917126fe7b470724
raphsilva <raphaelsilva2500@gmail.com>
2019-10-06 19:10:31 -0300

    Refactored.

--- Statistic/evaluate.py
+++ Statistic/evaluate.py
@@ -1,107 +1,100 @@
 import metrics
 from setup import DISCARD_TESTS, REPEAT_TESTS
+from statistics import stdev
 
-hr = []
-hc = []
-hd = []
-hh = []
 
-h_words1 = []
-h_words2 = []
-h_sentences1 = []
-h_sentences2 = []
+r_scores = []
+c_scores = []
+d_scores = []
+h_scores = []
+
+qnt_words_1 = []
+qnt_words_2 = []
+qnt_sentences_1 = []
+qnt_sentences_2 = []
 
 
 def reset():
-    global hr, hc, hd, hh, h_words1, h_words2, h_sentences1, h_sentences2
-    hr = []
-    hc = []
-    hd = []
-    hh = []
-
-    h_words1 = []
-    h_words2 = []
-    h_sentences1 = []
-    h_sentences2 = []
+    global r_scores, c_scores, d_scores, h_scores, qnt_words_1, qnt_words_2, qnt_sentences_1, qnt_sentences_2
+    r_scores = []
+    c_scores = []
+    d_scores = []
+    h_scores = []
+    qnt_words_1 = []
+    qnt_words_2 = []
+    qnt_sentences_1 = []
+    qnt_sentences_2 = []
 
 
 def new_sample(source1, source2, summ1, summ2):
-    global hr, hc, hd, hh, h_words1, h_words2, h_sentences1, h_sentences2
-    evals = {}
+    global r_scores, c_scores, d_scores, h_scores, qnt_words_1, qnt_words_2, qnt_sentences_1, qnt_sentences_2
+    scores = {}
 
-    evals['r1'] = 100 * metrics.representativiness(source1, summ1)
-    evals['r2'] = 100 * metrics.representativiness(source2, summ2)
-    evals['R'] = (evals['r1'] + evals['r2']) / 2
+    scores['r1'] = 100 * metrics.representativiness(source1, summ1)
+    scores['r2'] = 100 * metrics.representativiness(source2, summ2)
+    scores['R'] = (scores['r1'] + scores['r2']) / 2
 
-    evals['C'] = 100 * metrics.contrastiviness(source1, source2, summ1, summ2)
+    scores['C'] = 100 * metrics.contrastiviness(source1, source2, summ1, summ2)
 
-    evals['d1'] = 100 * metrics.diversity(source1, summ1)
-    evals['d2'] = 100 * metrics.diversity(source2, summ2)
+    scores['d1'] = 100 * metrics.diversity(source1, summ1)
+    scores['d2'] = 100 * metrics.diversity(source2, summ2)
 
-    evals['D'] = (evals['d1'] + evals['d2']) / 2
+    scores['D'] = (scores['d1'] + scores['d2']) / 2
 
-    evals['H'] = harmonic_mean([evals['R'], evals['C'], evals['D']])
+    scores['H'] = harmonic_mean([scores['R'], scores['C'], scores['D']])
 
     w1 = sum([summ1[i]['word_count'] for i in summ1])
     w2 = sum([summ2[i]['word_count'] for i in summ2])
 
-    h_words1.append(w1)
-    h_words2.append(w2)
-    h_sentences1.append(len(summ1))
-    h_sentences2.append(len(summ2))
+    qnt_words_1.append(w1)
+    qnt_words_2.append(w2)
+    qnt_sentences_1.append(len(summ1))
+    qnt_sentences_2.append(len(summ2))
 
-    hr.append(evals['R'])
-    hc.append(evals['C'])
-    hd.append(evals['D'])
-    hh.append(evals['H'])
+    r_scores.append(scores['R'])
+    c_scores.append(scores['C'])
+    d_scores.append(scores['D'])
+    h_scores.append(scores['H'])
 
-    return evals
+    return scores
 
 
 def overall_samples(SOURCE1, SOURCE2, exec_code, time_total, all_summaries):
-    from statistics import stdev
-
+    
     TABLE_RESULTS_FILENAME = 'RESULTS/table_results_' + exec_code + '.txt'  # Name of file that will save the results
 
-    hr_medians = sorted(hr)[DISCARD_TESTS:-DISCARD_TESTS]
-    hc_medians = sorted(hc)[DISCARD_TESTS:-DISCARD_TESTS]
-    hd_medians = sorted(hd)[DISCARD_TESTS:-DISCARD_TESTS]
-    hh_medians = sorted(hh)[DISCARD_TESTS:-DISCARD_TESTS]
+    r_scores_without_outliers = sorted(r_scores)[DISCARD_TESTS:-DISCARD_TESTS]
+    c_scores_without_outliers = sorted(c_scores)[DISCARD_TESTS:-DISCARD_TESTS]
+    d_scores_without_outliers = sorted(d_scores)[DISCARD_TESTS:-DISCARD_TESTS]
+    h_scores_without_outliers = sorted(h_scores)[DISCARD_TESTS:-DISCARD_TESTS]
 
-    sthh = stdev(hh_medians)
-    sthc = stdev(hc_medians)
-    sthr = stdev(hr_medians)
-    sthd = stdev(hd_medians)
+    r_stdev = stdev(r_scores_without_outliers)
+    c_stdev = stdev(c_scores_without_outliers)
+    d_stdev = stdev(d_scores_without_outliers)
+    h_stdev = stdev(h_scores_without_outliers)
 
-    r = sum(hr) / len(hr)
-    c = sum(hc) / len(hc)
-    d = sum(hd) / len(hd)
-    h = harmonic_mean([r, c, d])
-
-    r_median_mean = sum(hr_medians) / len(hr_medians)
-    c_median_mean = sum(hc_medians) / len(hc_medians)
-    d_median_mean = sum(hd_medians) / len(hd_medians)
-    h_median_mean = harmonic_mean([r, c, d])
-
-    ht = sum(hh) / len(hh)
+    r_mean = sum(r_scores_without_outliers) / len(r_scores_without_outliers)
+    c_mean = sum(c_scores_without_outliers) / len(c_scores_without_outliers)
+    d_mean = sum(d_scores_without_outliers) / len(d_scores_without_outliers)
+    h_mean = harmonic_mean([r_mean, c_mean, d_mean])
 
     results_msg = 'SCORES'
     results_msg += '\n\n'
     results_msg += '                R     C     D   harm mean '
     results_msg += '\n\n'
-    results_msg += 'mean          %3.0lf   %3.0lf   %3.0lf   [ %3.0lf ]' % (r_median_mean, c_median_mean, d_median_mean, h_median_mean)
+    results_msg += 'mean          %3.0lf   %3.0lf   %3.0lf   [ %3.0lf ]' % (r_mean, c_mean, d_mean, h_mean)
     results_msg += '\n\n'
-    results_msg += 'stdevs       ~%3.0lf  ~%3.0lf  ~%3.0lf    ~%3.0lf' % (sthr, sthc, sthd, sthh)
+    results_msg += 'stdevs       ~%3.0lf  ~%3.0lf  ~%3.0lf    ~%3.0lf' % (r_stdev, c_stdev, d_stdev, h_stdev)
     results_msg += '\n\n\n'
-    results_msg += 'max           %3.0lf   %3.0lf   %3.0lf     %3.0lf' % ((max(hr_medians)), (max(hc_medians)), (max(hd_medians)), (max(hh_medians)))
+    results_msg += 'max           %3.0lf   %3.0lf   %3.0lf     %3.0lf' % ((max(r_scores_without_outliers)), (max(c_scores_without_outliers)), (max(d_scores_without_outliers)), (max(h_scores_without_outliers)))
     results_msg += '\n\n'
-    results_msg += 'min           %3.0lf   %3.0lf   %3.0lf     %3.0lf' % ((min(hr_medians)), (min(hc_medians)), (min(hd_medians)), (min(hh_medians)))
+    results_msg += 'min           %3.0lf   %3.0lf   %3.0lf     %3.0lf' % ((min(r_scores_without_outliers)), (min(c_scores_without_outliers)), (min(d_scores_without_outliers)), (min(h_scores_without_outliers)))
     results_msg += '\n\n\n'
 
-    avg_words1 = sum(h_words1) / len(h_words1)
-    avg_words2 = sum(h_words2) / len(h_words2)
-    avg_sentences1 = sum(h_sentences1) / len(h_sentences1)
-    avg_sentences2 = sum(h_sentences2) / len(h_sentences2)
+    avg_words1 = sum(qnt_words_1) / len(qnt_words_1)
+    avg_words2 = sum(qnt_words_2) / len(qnt_words_2)
+    avg_sentences1 = sum(qnt_sentences_1) / len(qnt_sentences_1)
+    avg_sentences2 = sum(qnt_sentences_2) / len(qnt_sentences_2)
 
     results_msg += '\n\n'
     results_msg += ' avg words 1:  %6.2lf ' % (avg_words1)
@@ -128,7 +121,7 @@ def overall_samples(SOURCE1, SOURCE2, exec_code, time_total, all_summaries):
     f.write('\n\n\n\n\n\n')
     f.close()
 
-    return [r_median_mean, c_median_mean, d_median_mean]
+    return [r_mean, c_mean, d_mean]
 
 
 def harmonic_mean(l):
