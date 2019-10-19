2aebb926d9919cfdaded2e3406acc05d36d1e9eb
raphsilva <raphaelsilva2500@gmail.com>
2019-10-19 18:54:55 -0300

    Reformatted.

--- Clustering/setup.py
+++ Clustering/setup.py
@@ -1,17 +1,32 @@
-LIMIT_SENTENCES = 'Auto'  # Summary length (number of sentences on each side). Set 'Auto' for automatic.
-
-LIMIT_WORDS = 100
-
-LANGUAGE = ['portuguese'][0]
+# Method options
 
 METHOD = ['RF', 'CF'][0]
 LAMBDA = 0.5
-PICK_CENTROIDS = False
-HUNGARIAN_METHOD = False
+PICK_CENTROIDS = False  #
+HUNGARIAN_METHOD = False  # Whether the Hungarian Method should be used for optimization.
+SIZE_FAC = 0.7  # To help control the summary size (since this method doesn't allow to directly control number of words)
+
+
+# Summary characteristics
+
+LIM_WORDS = 100  # Sets the maximum number of words in each side of the summary
+LIMIT_SENTENCES = 'Auto'  # Sets the number of sentences on each side. Use 'Auto' for automatic.
+
 ALLOW_REPETITION = False
+
+
+# Evaluation options
 
 REPEAT_TESTS = 10  # Times to repeat each test
 DISCARD_TESTS = int(0.1*REPEAT_TESTS)  # Amount of top-rated and bottom-rated tests to be discarded to compute average
+
+
+# Display options
+
+VERBOSE_MODE = True
+
+
+# Input files settings
 
 DATA_DIR = '../input'
 
@@ -19,11 +34,8 @@ INPUT_FILES = [(), ('D1a','D1b'), ('D2a','D2b'), ('D3a','D3b'), ('D4a','D4b'), (
 
 DATASETS_TO_TEST = [INPUT_FILES[2]]
 
-VERBOSE_MODE = True
-
-SIZE_FAC = 0.7
-
-def filepath(target):
+def filepath(target):  # Get full path for the file with data of target
     return DATA_DIR + '/' + target + '.json'
 
+LANGUAGE = ['portuguese'][0]
 
