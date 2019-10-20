e24c832a01476501d5a0355afab36aeb12a0246a
raphsilva <raphaelsilva2500@gmail.com>
2019-10-19 23:27:20 -0300

    Download lemma dictionary.

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,4 +1,5 @@
 POS_PT='http://nilc.icmc.usp.br/nlpnet/data/pos-pt.tgz'
+LEMMAS='http://www.nilc.icmc.usp.br/nilc/projects/unitex-pb/web/files/DELAF_PB_v2.zip'
 
 echo 'Making directories'
 mkdir Clustering/language
@@ -7,9 +8,12 @@ cd Clustering/language/portuguese
 
 echo 'Downloading files'
 wget $POS_PT
+wget $LEMMAS
 
 echo 'Extracting files'
 tar -xzf pos-pt.tgz --strip-components 1
+unzip DELAF_PB_v2.zip
 
 echo 'Cleaning files'
 gio trash pos-pt.tgz
+gio trash DELAF_PB_v2
